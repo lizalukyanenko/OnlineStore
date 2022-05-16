73e3ee2630ecb8429e1f291992b199494faeacd9
lizamarmysh <liza.marmysh1999@gmail.com>
2022-05-16 12:34:51 +0300

    Functionality for opening/closing a modal window is ready

--- js/script.js
+++ js/script.js
@@ -73,6 +73,11 @@ let product;
 
 let products = [];
 
+let productIndex;
+
+const productList = document.querySelector(`.results__list`);
+const popup = document.querySelector(`.popup`);
+
 function randomProduct(){
   return product = {
     name: productName[Math.floor(Math.random()*productName.length)],
@@ -105,13 +110,12 @@ function randomDate(start, end) {
 function createProducts(){
   for (let i = 0; i < 7; i++) {
     products.push(randomProduct());
-  }
-}
+  };
+};
 
 createProducts();
 
 function getProductList(products){
-  const productList = document.querySelector(`.results__list`);
   const productMarkup = products.map(productItem =>
     `<li class="results__item product">
     <button class="product__favourite fav-add" type="button" aria-label="Добавить в избранное">
@@ -127,12 +131,111 @@ function getProductList(products){
       <h3 class="product__title">
         <a href="#">${productItem.name}</a>
       </h3>
-      <div class="product__price">${productItem.price}₽</div>
+      <div class="product__price">${productItem.price} ₽</div>
       <div class="product__address">${productItem.address.city}, ${productItem.address.street}</div>
       <div class="product__date">${productItem.publishDate.getFullYear()}</div>
     </div>
   </li>`).join(``);
   productList.innerHTML = ``;
   productList.insertAdjacentHTML(`afterbegin`,productMarkup);
-}
+};
 getProductList(products);
+
+
+function onProductClick(evt){
+  const productElem = evt.target.closest(`li`);
+  if (!productElem){
+    return;
+  };
+  // надо как-то передать сам элемент массива
+  // через productElement передается разметка
+  renderPopup(products[1]);
+  popup.style.display = `block`;
+
+  const popupClose = popup.querySelector(`.popup__close`);
+  popupClose.addEventListener(`click`, onClosePopupClick);
+};
+
+function renderPopup(product){
+  const currentPopup = `<div class="popup__inner">
+    <button class="popup__close" type="button" aria-label="Закрыть">
+      <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683418 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L8 6.58579L14.2929 0.292893C14.6834 -0.0976311 15.3166 -0.0976311 15.7071 0.292893C16.0976 0.683418 16.0976 1.31658 15.7071 1.70711L9.41421 8L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L8 9.41421L1.70711 15.7071C1.31658 16.0976 0.683418 16.0976 0.292893 15.7071C-0.0976311 15.3166 -0.0976311 14.6834 0.292893 14.2929L6.58579 8L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683418 0.292893 0.292893Z"/>
+      </svg>
+    </button>
+    <div class="popup__date">${product.publishDate.getFullYear()}</div>
+    <h3 class="popup__title">${product.name}</h3>
+    <div class="popup__price">${product.price} ₽</div>
+    <div class="popup__columns">
+      <div class="popup__left">
+        <div class="popup__gallery gallery">
+          <button class="gallery__favourite fav-add">
+            <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M3 7C3 13 10 16.5 11 17C12 16.5 19 13 19 7C19 4.79086 17.2091 3 15 3C12 3 11 5 11 5C11 5 10 3 7 3C4.79086 3 3 4.79086 3 7Z" stroke="white" stroke-width="2" stroke-linejoin="round"/>
+            </svg>
+          </button>
+          <div class="gallery__main-pic">
+            <img src="${product.photos}" width="520" height="340" alt="${product.name}">
+          </div>
+          <ul class="gallery__list">
+            <li class="gallery__item gallery__item--active">
+              <img src="${product.photos}" width="124" height="80" alt="${product.name}">
+            </li>
+            <li class="gallery__item">
+              <img src="${product.photos}" width="124" height="80" alt="${product.name}">
+            </li>
+            <li class="gallery__item">
+              <img src="${product.photos}" width="124" height="80" alt="${product.name}">
+            </li>
+            <li class="gallery__item">
+              <img src="${product.photos}" width="124" height="80" alt="${product.name}">
+            </li>
+          </ul>
+        </div>
+        <ul class="popup__chars chars">
+          <li class="chars__item">
+            <div class="chars__name">Площадь</div>
+            <div class="chars__value">${product.area}</div>
+          </li>
+          <li class="chars__item">
+            <div class="chars__name">Количество комнат</div>
+            <div class="chars__value">${product.roomsCount}</div>
+          </li>
+          <li class="chars__item">
+            <div class="chars__name">Тип недвижимости</div>
+            <div class="chars__value">${product.type}</div>
+          </li>
+        </ul>
+        <div class="popup__seller seller seller--good">
+          <h3>Продавец</h3>
+          <div class="seller__inner">
+            <a class="seller__name" href="#">${product.seller.fullname}</a>
+            <div class="seller__rating"><span>${product.seller.rating}</span></div>
+          </div>
+        </div>
+        <div class="popup__description">
+          <h3>Описание товара</h3>
+          <p>${product.description}</p>
+        </div>
+      </div>
+      <div class="popup__right">
+        <div class="popup__map">
+          <img src="img/map.jpg" width="268" height="180" alt="${product.address.city}, ${product.address.street}, дом ${product.address.building}">
+        </div>
+        <div class="popup__address">${product.address.city}, ${product.address.street}, дом ${product.address.building}</div>
+      </div>
+    </div>
+  </div>`;
+  popup.innerHTML = ``;
+  popup.insertAdjacentHTML(`afterbegin`, currentPopup);
+};
+
+function onClosePopupClick(){
+  popup.style.display = `none`;
+};
+
+
+
+productList.addEventListener(`click`, onProductClick);
+
+
